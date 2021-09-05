a0fea8d8278b16d488adbadc57512ee98ac1ace0
aasish_mahato <aasish.mahato101@gmail.com>
2021-09-05 11:59:14 +0545

    Airbnb-cloned successfully

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 px-32 space-y-10 text-gray-600 bg-gray-100 md:grid-cols-4 py-14">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>There is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Papafam</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>papa React</p>
+        <p>Presents</p>
+        <p>Zero to Full Stack Hero</p>
+        <p>Hundreds of Students</p>
+        <p>Join Now</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi Youtube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-md md:px-10">
+      {/* left */}
+      <div className="relative flex items-center h-10 cursor-pointer md:shadow-sm">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      {/* middle */}
+      <div className="flex items-center py-2 rounded-full md:border-2">
+        <input
+          className="flex-grow pl-2 text-sm text-gray-600 placeholder-gray-400 bg-transparent outline-none"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden h-8 p-2 text-white bg-red-400 rounded-full cursor-pointer md:inline-flex md:mx-2" />
+      </div>
+      {/* right */}
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+          <p className="hidden cursor-pointer md:inline-flex">Become a host</p>
+          <GlobeAltIcon className="h-6 cursor-pointer" />
+          <div className="flex items-center p-2 space-x-2 border-2 rounded-full">
+              <MenuIcon className="h-6" />
+              <UserCircleIcon className="h-6" />
+          </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
